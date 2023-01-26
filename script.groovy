3c7a82ef102e3850a2f61af9983fcf7e70e2a8e4
Yassine <107377661+ZERQQAOUI@users.noreply.github.com>
2023-01-26 16:26:46 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,7 +6,7 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+        sh 'docker build -t zerqqaoui/text_to_binary_java_maven:jma-1.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker zerqqaoui/text_to_binary_java_maven:jma-1.0'
     }
